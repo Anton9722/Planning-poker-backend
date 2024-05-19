4a2dfce7db1fec0fd57b65e19cadb53ccdf7c683
Roberto Tencic <roberto.tencic@outlook.com>
2024-05-19 20:54:45 +0200

    Uppdatering.

--- src/main/java/com/PlanningPoker/PlanningPoker/Controllers/IssueController.java
+++ src/main/java/com/PlanningPoker/PlanningPoker/Controllers/IssueController.java
@@ -1,9 +1,10 @@
 package com.PlanningPoker.PlanningPoker.Controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import com.PlanningPoker.PlanningPoker.models.Issue;
 import com.PlanningPoker.PlanningPoker.service.IssueService;
-import java.util.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @CrossOrigin("*")
 @RestController
@@ -14,33 +15,70 @@ public class IssueController {
         this.issueService = issueService;
     }
 
+    // Returnerar alla issues från ett projekt.
     @GetMapping("project/{projectId}/issues")
-    public List<Issue> getProjectIssues(@PathVariable UUID projectId) {
-        return issueService.getProjectIssues(projectId);
+    public ResponseEntity<?> getProjectIssues(
+            @PathVariable("projectId") String projectId,
+            @RequestHeader("sessionID") String sessionId) {
+        return issueService.getProjectIssues(projectId, sessionId);
     }
 
+    // Returnerar ett issue.
     @GetMapping("project/issue/{issueId}")
-    public Issue getIssueById(@PathVariable UUID issueId) {
-        return issueService.getIssueById(issueId);
+    public ResponseEntity<?> getIssueById(
+            @PathVariable("issueId") String issueId,
+            @RequestHeader("sessionID") String sessionId) {
+        return issueService.getIssueById(issueId, sessionId);
     }
 
-    @PostMapping("/issue")
-    public Issue createIssue(@RequestBody Issue issue) {
-        return issueService.createIssue(issue);
+    // Skapar ett issue.
+    @PostMapping("project/issue")
+    public ResponseEntity<?> createIssue(
+            @RequestBody Issue issue,
+            @RequestHeader("sessionID") String sessionId) {
+        return issueService.createIssue(issue, sessionId);
     }
 
-    @DeleteMapping("/issue/{issueId}")
-    public Issue deleteIssue(@PathVariable UUID issueId) {
-        return issueService.deleteIssue(issueId);
+    // Tar bort ett issue.
+    @DeleteMapping("project/issue/{issueId}")
+    public ResponseEntity<?> deleteIssue(
+            @PathVariable("issueId") String issueId,
+            @RequestHeader("sessionID") String sessionId) {
+        return issueService.deleteIssue(issueId, sessionId);
     }
 
-    @PostMapping("/issue/{issueId}")
-    public Issue estimateTime(@PathVariable UUID issueId) {
-        return issueService.estimateTime(issueId);
+    // Tilldelar en medlem till ett issue.
+    @PatchMapping("project/issue/assign-member/{issueId}")
+    public ResponseEntity<?> assignIssue(
+            @PathVariable("issueId") String issueId,
+            @RequestHeader("userId") String userId,
+            @RequestHeader("sessionID") String sessionId) {
+        return issueService.assignIssue(userId, issueId, sessionId);
     }
 
-    @PostMapping("/issue/{issueId}")
-    public Issue closeIssue(@PathVariable UUID issueId) {
-        return issueService.closeIssue(issueId);
+    // Tilldelar en tid till ett issue.
+    @PatchMapping("project/issue/assign-time/{issueId}")
+    public ResponseEntity<?> assignTime(
+            @PathVariable("issueId") String issueId,
+            @RequestHeader("userId") String userId,
+            @RequestHeader("estimatedTime") int estimatedTime,
+            @RequestHeader("sessionID") String sessionId) {
+        return issueService.assignTime(userId, issueId, estimatedTime, sessionId);
+    }
+
+    // Stänger ett issue.
+    @PatchMapping("project/issue/close/{issueId}")
+    public ResponseEntity<?> closeIssue(
+            @PathVariable("issueId") String issueId,
+            @RequestHeader("sessionID") String sessionId) {
+        return issueService.closeIssue(issueId, sessionId);
+    }
+
+    // Returnerar medelvärdet för estimerad tid.
+    @GetMapping("project/issue/estimatedTime/{issueId}")
+    public ResponseEntity<?> getEstimatedTime(
+            @PathVariable("issueId") String issueId,
+            @RequestHeader("sessionID") String sessionId) {
+        return issueService.getEstimatedTime(issueId, sessionId);
     }
 }
--- src/main/java/com/PlanningPoker/PlanningPoker/service/IssueService.java
+++ src/main/java/com/PlanningPoker/PlanningPoker/service/IssueService.java
@@ -1,39 +1,99 @@
 package com.PlanningPoker.PlanningPoker.service;
 
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.*;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import com.PlanningPoker.PlanningPoker.models.*;
 
-import com.PlanningPoker.PlanningPoker.models.Issue;
-
+@Service
 public class IssueService {
-    private MongoOperations mongoOperations;
+    private final MongoOperations mongoOperations;
 
     public IssueService(MongoOperations mongoOperations) {
         this.mongoOperations = mongoOperations;
     }
 
-    public List<Issue> getProjectIssues(UUID projectId) {
-        return null;
+    // Returnerar alla issues från ett projekt.
+    public ResponseEntity<?> getProjectIssues(String projectId, String sessionId) {
+        List<Issue> issues = mongoOperations.findAll(Issue.class).stream()
+                .filter((Issue issue) -> issue.getProjectId().equals(projectId)).toList();
+        return !issues.isEmpty()
+                ? ResponseEntity.ok().body(issues)
+                : ResponseEntity.status(HttpStatus.NOT_FOUND).body("No issues found.");
     }
 
-    public Issue getIssueById(UUID issueId) {
-        return null;
+    // Returnerar ett issue.
+    public ResponseEntity<?> getIssueById(String issueId, String sessionId) {
+        return mongoOperations.findOne(new Query(Criteria.where("id").is(issueId)), Issue.class) != null
+                ? ResponseEntity.ok().body(issueId)
+                : ResponseEntity.status(HttpStatus.NOT_FOUND).body("Issue not found.");
     }
 
-    public Issue createIssue(Issue issue) {
-        return null;
+    // Skapar ett issue.
+    public ResponseEntity<?> createIssue(Issue issue, String sessionId) {
+        return mongoOperations.insert(issue) != null
+                ? ResponseEntity.ok().body(issue)
+                : ResponseEntity.status(HttpStatus.NOT_MODIFIED).body("Issue not created.");
     }
 
-    public Issue deleteIssue(UUID issueId) {
-        return null;
+    // Tar bort ett issue.
+    public ResponseEntity<?> deleteIssue(String issueId, String sessionId) {
+        return mongoOperations.remove(new Query(Criteria.where("id").is(issueId)), Issue.class).getDeletedCount() > 0
+                ? ResponseEntity.ok().body(issueId)
+                : ResponseEntity.status(HttpStatus.NOT_FOUND).body("Issue not found.");
     }
 
-    public Issue estimateTime(UUID issueId) {
-        return null;
+    // Tilldelar en medlem till ett issue.
+    public ResponseEntity<?> assignIssue(String userId, String issueId, String sessionId) {
+        Issue issue = mongoOperations.findOne(new Query(Criteria.where("id").is(issueId)), Issue.class);
+        if (issue != null) {
+            issue.getAssignedIds().add(userId);
+            mongoOperations.updateFirst(new Query(Criteria.where("id").is(issueId)),
+                    Update.update("assignedIds", issue.getAssignedIds()), Issue.class);
+            return ResponseEntity.ok().body(issue);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Issue not found.");
+        }
     }
 
-    public Issue closeIssue(UUID issueId) {
-        return null;
+    // Sätter estimerad tid.
+    public ResponseEntity<?> assignTime(String userId, String issueId, int estimatedTime, String sessionId) {
+        Issue issue = mongoOperations.findOne(new Query(Criteria.where("id").is(issueId)), Issue.class);
+        if (issue != null) {
+            issue.getEstimatedTimes().put(userId, estimatedTime);
+            mongoOperations.updateFirst(new Query(Criteria.where("id").is(issueId)),
+                    Update.update("estimatedTimes", issue.getEstimatedTimes()), Issue.class);
+            return ResponseEntity.ok().body(issue);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Issue not found.");
+        }
+    }
+
+    // Avslutar ett issue. Sätter isDone till true.
+    public ResponseEntity<?> closeIssue(String issueId, String sessionId) {
+        Issue issue = mongoOperations.findOne(new Query(Criteria.where("id").is(issueId)), Issue.class);
+        if (issue != null) {
+            mongoOperations.updateFirst(new Query(Criteria.where("id").is(issueId)), Update.update("isDone", true),
+                    Issue.class);
+            return ResponseEntity.ok().body(issueId);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Issue not found.");
+        }
+    }
+
+    // Returnerar medelvärdet för estimerad tid.
+    public ResponseEntity<?> getEstimatedTime(String issueId, String sessionId) {
+        Issue issue = mongoOperations.findOne(new Query(Criteria.where("id").is(issueId)), Issue.class);
+        if (issue != null) {
+            int estimatedTime = 0;
+            for (int i : issue.getEstimatedTimes().values()) {
+                estimatedTime += i;
+            }
+            return ResponseEntity.ok().body(estimatedTime / issue.getEstimatedTimes().size());
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Issue not found.");
+        }
     }
 }
